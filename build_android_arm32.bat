2042c101b4c564c134ebd8467188759e3924f536
jing332 <3033784236@qq.com>
2022-09-12 10:07:33 +0800

    Actions自动Release

--- build_android_arm32.bat
+++
@@ -1,8 +0,0 @@
-SET GOOS=android
-SET GOARCH=arm
-SET CGO_ENABLED=1
-SET CC=E:\Android_NDK\toolchains-r20\bin\arm-linux-androideabi-gcc
-@REM SET CC=E:\Android_NDK\android-ndk-r24\toolchains\llvm\prebuilt\windows-x86_64\bin\armv7a-linux-androideabi19-clang.cmd
-go build -buildvcs=false -o output/tts-server-go_arm32
-
-pause
--- build_android_arm64.bat
+++
@@ -1,8 +0,0 @@
-SET GOOS=android
-SET GOARCH=arm64
-SET CGO_ENABLED=1
-SET CC=E:\Android_NDK\android-ndk-r24\toolchains\llvm\prebuilt\windows-x86_64\bin\aarch64-linux-android23-clang.cmd
-
-go build -buildvcs=false -o output/tts-server-go_arm64
-
-pause
