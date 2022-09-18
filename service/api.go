66f9ece7f0d52124523d1a3aa81c1f131c66f4db
jing332 <3033784236@qq.com>
2022-09-18 21:20:33 +0800

    Azure网页选项汉化 & 添加角色(身份)选择

--- service/api.go
+++ service/api.go
@@ -166,9 +166,10 @@ func (s *GracefulServer) legadoAPIHandler(w http.ResponseWriter, r *http.Request
 	voiceName := params.Get("voiceName")
 	styleName := params.Get("styleName")
 	styleDegree := params.Get("styleDegree")
+	roleName := params.Get("roleName")
 	voiceFormat := params.Get("voiceFormat")
 
-	jsonStr, err := genLegodoJson(apiUrl, name, voiceName, styleName, styleDegree, voiceFormat)
+	jsonStr, err := genLegodoJson(apiUrl, name, voiceName, styleName, styleDegree, roleName, voiceFormat)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte(err.Error()))
@@ -177,7 +178,7 @@ func (s *GracefulServer) legadoAPIHandler(w http.ResponseWriter, r *http.Request
 	w.Write(jsonStr)
 }
 
-func genLegodoJson(api, name, voiceName, styleName, styleDegree, format string) ([]byte, error) {
+func genLegodoJson(api, name, voiceName, styleName, styleDegree, roleName, voiceFormat string) ([]byte, error) {
 	t := time.Now().UnixNano() / 1e6 //毫秒时间戳
 	var url string
 	if styleName == "" {
@@ -185,11 +186,11 @@ func genLegodoJson(api, name, voiceName, styleName, styleDegree, format string) 
 			voiceName + `\"><prosody rate=\"{{(speakSpeed -10) * 2}}%\" pitch=\"+0Hz\">{{String(speakText).replace(/&/g, '&amp;').replace(/\"/g, '&quot;').replace(/'/g, '&apos;').replace(/</g, '&lt;').replace(/>/g, '&gt;')}}</prosody></voice></speak>"}`
 	} else {
 		url = api + ` ,{"method":"POST","body":"<speak xmlns=\"http://www.w3.org/2001/10/synthesis\" xmlns:mstts=\"http://www.w3.org/2001/mstts\" xmlns:emo=\"http://www.w3.org/2009/10/emotionml\" version=\"1.0\" xml:lang=\"en-US\"><voice name=\"` +
-			voiceName + `\"><mstts:express-as style=\"` + styleName + `\" styledegree=\" ` + styleDegree + `\"><prosody rate=\"{{(speakSpeed -10) * 2}}%\" pitch=\"+0Hz\">{{String(speakText).replace(/&/g, '&amp;').replace(/\"/g, '&quot;').replace(/'/g, '&apos;').replace(/</g, '&lt;').replace(/>/g, '&gt;')}}</prosody> </mstts:express-as></voice></speak>"}`
+			voiceName + `\"><mstts:express-as style=\"` + styleName + `\" styledegree=\"` + styleDegree + `\" role=\"` + roleName + `\"><prosody rate=\"{{(speakSpeed -10) * 2}}%\" pitch=\"+0Hz\">{{String(speakText).replace(/&/g, '&amp;').replace(/\"/g, '&quot;').replace(/'/g, '&apos;').replace(/</g, '&lt;').replace(/>/g, '&gt;')}}</prosody> </mstts:express-as></voice></speak>"}`
 	}
 
-	head := `{"Content-Type":"text/plain","Authorization":"Bearer ","Format":"` + format + `"}`
-	legadoJson := &LegadoJson{Name: name, URL: url, ID: t, LastUpdateTime: t, ContentType: formatContentType(format), Header: head}
+	head := `{"Content-Type":"text/plain","Authorization":"Bearer ","Format":"` + voiceFormat + `"}`
+	legadoJson := &LegadoJson{Name: name, URL: url, ID: t, LastUpdateTime: t, ContentType: formatContentType(roleName), Header: head}
 
 	body, err := json.Marshal(legadoJson)
 	if err != nil {
